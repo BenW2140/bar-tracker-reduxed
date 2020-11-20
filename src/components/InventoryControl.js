4aac162c1dcca3a4828101f7cea597c24198e015
Ben White <bwhite2140@outlook.com>
2020-11-20 11:07:57 -0800

    add constructor and render for inventory control

--- src/components/InventoryControl.js
+++ src/components/InventoryControl.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-function InventoryControl() {
+class InventoryControl extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnSite: false,
+      masterListOfBrews: [],
+      selectedBrew: null
+    }
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick = {this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default InventoryControl;
