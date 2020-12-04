9f72702ccefd0a14e91f9f92d7b6aa95be23462c
Ben White <bwhite2140@outlook.com>
2020-12-04 14:11:19 -0800

    add failing test for creating toggle action

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -5,6 +5,11 @@ describe('bar tracker actions', () => {
     expect(actions.deleteBrew(1)).toEqual({
       type: 'DELETE_BREW',
       id: 1
-    })
+    });
+  });
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
   });
 });
