2e5de6a1affc8d73272fd6de00be989cac51a5f7
Ben White <bwhite2140@outlook.com>
2020-12-04 13:06:55 -0800

    add failing test for toggling form visibility

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,8 +1,11 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 
 describe('formVisibleReducer', () => {
-  
   test('Should return default state if no action is passed', () => {
     expect (formVisibleReducer(false, { type: null })).toEqual(false);
+  });
+
+  test('should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
   });
 });
