cbdb7d5fa2aa64ec66c8dc0aaeda22758cf07d08
Ben White <bwhite2140@outlook.com>
2020-12-04 16:35:13 -0800

    add failing test for returning default state for editing

--- src/__tests__/reducers/edit-brew-reducer.test.js
+++ src/__tests__/reducers/edit-brew-reducer.test.js
@@ -1,5 +1,7 @@
 import editBrewReducer from '../../reducers/edit-brew-reducer';
 
 describe('editBrewReducer', () => {
-  
+  test('should return default state if no action is passed', () => {
+    expect(editBrewReducer(false, { type: null })).toEqual(false);
+  })
 });
